b06760753be7f3558828da2b2c16b0034dfc034b
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-01-11 16:17:59 -0200

    Add Complaints::Fields

---
+++ app/models/complaints/fields.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Complaints
+  module Fields
+    ALL = [
+      TITLE = 'title',
+      LOCALE = 'locale',
+      COMPANY = 'company',
+      DESCRIPTION = 'description'
+    ].freeze
+  end
+end
