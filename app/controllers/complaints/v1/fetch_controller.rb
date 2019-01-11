5dd7d62a73a1a1a3d4927f4d5e0a6ae444950aa0
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-01-11 21:56:06 -0200

    Refactor Complaints::V1::FetchController

--- app/controllers/complaints/v1/fetch_controller.rb
+++ app/controllers/complaints/v1/fetch_controller.rb
@@ -4,29 +4,15 @@ module Complaints
   module V1
     class FetchController < ApplicationController
       def call
-        result = map_complaint_data(record: find_record, fields: Fields::ALL)
+        record = Document.find(params[:id])
 
-        render status: 200, json: result
+        serialize = Serializer.new(Fields::ALL)
+
+        render status: 200, json: serialize.call(record)
       rescue Mongoid::Errors::DocumentNotFound => e
         render status: :not_found, json: {
           error: ["Complaint not found with id: \"#{params[:id]}\""]
         }
-      end
-
-      private
-
-      def find_record
-        Document.find(params[:id])
-      end
-
-      def map_complaint_data(record:, fields:)
-        { id: record.id.to_s }.tap do |result|
-          fields.each do |field|
-            value = record[field]
-
-            result[field] = value if value.present?
-          end
-        end
       end
     end
   end
