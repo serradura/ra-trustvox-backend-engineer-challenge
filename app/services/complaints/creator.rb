e649999e64fccf9427ee4db25983838ee9fa568b
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-01-13 22:22:27 -0200

    Add Complaints::Creator service
    ... and refactor complaints/v1/create_controller applying it.

---
+++ app/services/complaints/creator.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Complaints
+  class Creator
+    RESULTS = [
+      FAILURE = :failure,
+      SUCCESS = :created
+    ].freeze
+
+    def initialize(params)
+      @params = params
+    end
+
+    def call
+      return FAILURE if some_required_param_unfilled?
+
+      create_later!
+        .then { SUCCESS }
+    end
+
+    private
+
+    def some_required_param_unfilled?
+      @params.empty? || Fields::ALL.any? do |field|
+        @params[field].nil?
+      end
+    end
+
+    def create_later!
+      CreateJob.perform_later(@params.as_json)
+    end
+  end
+end
