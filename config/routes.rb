76458c0522aef7af65f1cc2e12d6d3cbad2c3d66
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-01-14 02:50:07 -0200

    Refactor routes with Routes::ModularResourcesMapper

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-require_relative './routes/json_constraint'
-require_relative './routes/mappers'
+require_relative './routes/modular_resources_mapper'
 
 Rails.application.routes.draw do
-  map_v1_resource = Routes::Mappers::MapV1Resource
-  map_base_resource = Routes::Mappers::MapBaseResource
+  extend Routes::ModularResourcesMapper
 
-  {
+  map_modular_json_resources!(
     v1: {
       complaints: {
         index: :fetch_all,
@@ -15,23 +13,7 @@ Rails.application.routes.draw do
         create: :create
       }
     }
-  }.each do |version, resource_names|
-    resource_names.each do |resource_name, scheme|
-      namespace version, module: resource_name, constraints: Routes::JSONConstraint do
-        scheme.each do |action, controller|
-          resources resource_name, map_v1_resource[action, controller: controller]
-        end
-      end
-    end
-
-    resource_names.each do |resource_name, scheme|
-      scope resource_name, module: resource_name do
-        scheme.each do |action, controller|
-          resources resource_name, map_base_resource[action, controller: controller]
-        end
-      end
-    end
-  end
+  )
 
   root to: 'errors/v1/not_found#call', via: :all
 
