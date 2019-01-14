45aa89b3e8fe115d210fa97bf05ba13a89a07afa
Rodrigo Serradura <rodrigo@ysimplicity.com>
2019-01-14 11:07:02 -0200

    Create a DSL to define modular resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
-require_relative './routes/modular_resources_mapper'
+require_relative './routes/json_constraint'
+require_relative './routes/modular_resources'
 
 Rails.application.routes.draw do
-  extend Routes::ModularResourcesMapper
+  extend Routes::ModularResources
 
-  map_modular_json_resources!(
-    v1: {
-      complaints: {
-        index: :index,
-        show: :show,
-        create: :create
-      }
-    }
-  )
+  modular_namespace [:v1, :complaints], constraints: Routes::JSONConstraint do
+    modular_resources only: %i[index show create]
+  end
+
+  modular_resources :complaints, only: %i[index show create],
+                                 module: :v1,
+                                 constraints: Routes::JSONConstraint
 
   root to: 'errors/v1/not_found#call', via: :all
 
